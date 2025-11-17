63d5cfa056072349d4f22a6d52cd0b2e57e60a0d
David Hocking <65630382+KiwiBandicoot@users.noreply.github.com>
2025-11-18 00:43:44 +1300

    Remove duplicate image URLs from pinguImages
    Removed duplicate image URLs from the pinguImages array.

--- Content.js
+++ Content.js
@@ -1,13 +1,14 @@
 let pinguImages = [
   "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
+  "https://tinybop.com/img/loves/pingu/pingu-hero.jpg",
+  "https://www.easyspot.co.nz/cdn/shop/files/89faff6708f40d94adad0d91f8ee598a.jpg?v=1747882499&width=823",
+  "https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABQoLuJydqligrJrjHoaI1IuiaVv5BOQ7Ed6Y5ADLEsyzXqwDPW12H7Ramu9ljp5xFcgAxUJw60gSpAyIjCt9PH7cq_NEGh5QsdUC.jpg?r=6ff",
+  "https://images.timesnownews.com/thumb/msid-114447996,thumbsize-21060,width-1280,height-720,resizemode-75/114447996.jpg",
   "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
-  "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
-  "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
-  "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
-  "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
-  "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
-  "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
-  "https://images.contentstack.io/v3/assets/bltc12136c3b9f23503/blt5f81916810015b49/67167cf1e43e4371989a0fcf/PIN01_07.jpg",
+  "https://tinybop.com/img/loves/pingu/pingu-hero.jpg",
+  "https://www.easyspot.co.nz/cdn/shop/files/89faff6708f40d94adad0d91f8ee598a.jpg?v=1747882499&width=823",
+  "https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABQoLuJydqligrJrjHoaI1IuiaVv5BOQ7Ed6Y5ADLEsyzXqwDPW12H7Ramu9ljp5xFcgAxUJw60gSpAyIjCt9PH7cq_NEGh5QsdUC.jpg?r=6ff",
+  "https://images.timesnownews.com/thumb/msid-114447996,thumbsize-21060,width-1280,height-720,resizemode-75/114447996.jpg",
 ];
 
 function replaceImages() {
